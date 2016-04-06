7dd646dda35bbda4c656f0bcdfede58abe70be7a
chenxijun <chenxijun@mirrtalk.com>
2016-04-06 20:02:09 +0800

    add ssdb spare

--- programs/RTAP/lua/dfsapi/code/spx_utils.lua
+++ programs/RTAP/lua/dfsapi/code/spx_utils.lua
@@ -8,23 +8,23 @@ function set_to_dfsdb(key, value, cacheTime)
 	local st, ed, type_val = string.find(key, "%.(%a%a.)$")
 	if type_val == 'amr' then
 		return redis_api.hash_cmd("dfsdb_amr_write", key, "set",key,value)
---		if cacheTime then
---			return redis_api.hash_cmd("dfsdb_amr_write","expire",key,cacheTime)
---		end
+		--		if cacheTime then
+		--			return redis_api.hash_cmd("dfsdb_amr_write","expire",key,cacheTime)
+		--		end
 	elseif type_val == 'mp4' then
 		return redis_api.hash_cmd("dfsdb_mp4_write", key, "set",key,value)
---		if cacheTime then
---			return redis_api.hash_cmd("dfsdb_mp4_write","expire",key,cacheTime)
---		end
+		--		if cacheTime then
+		--			return redis_api.hash_cmd("dfsdb_mp4_write","expire",key,cacheTime)
+		--		end
 	elseif type_val == 'jpg' then
 		return redis_api.hash_cmd("dfsdb_jpg_write", key, "set",key,value)
---		if cacheTime then
---			return redis_api.hash_cmd("dfsdb_jpg_write","expire",key,cacheTime)
---		end
+		--		if cacheTime then
+		--			return redis_api.hash_cmd("dfsdb_jpg_write","expire",key,cacheTime)
+		--		end
 	else
 		return redis_api.hash_cmd("dfsdb_file_write", key, "set",key,value)
 	end
-	
+
 end 
 
 function set_to_redis(key, value, cacheTime)
@@ -54,7 +54,7 @@ function set_to_redis(key, value, cacheTime)
 			return redis_api.hash_cmd("redis_file_write", key, "set",key,value)	
 		end
 	end
-	
+
 end 
 
 function get_from_dfsdb(key, group)
@@ -82,25 +82,80 @@ function get_from_redis(key, group)
 		return redis_api.just_cmd("redis_file_read", group, "get", key)
 	end
 end 
+
 function set_to_dfsdb_spare(key,value,cacheTime)
-	return redis_api.hash_cmd("dfsdb_write_spare", key, "set",key,value)		
---	if cacheTime then
---		return redis_api.hash_cmd("dfsdb_write_spare","expire",key,cacheTime)
---	end
+	-- return redis_api.hash_cmd("dfsdb_write_spare", key, "set",key,value)	 -- change to ssdb
+	local st, ed, type_val = string.find(key, "%.(%a%a.)$")
+	if type_val == 'amr' then
+		return redis_api.just_cmd("ssdb_amr_spare", key, "set", key,value)
+	elseif type_val == 'mp4' then
+		return redis_api.just_cmd("ssdb_mp4_spare", key, "set", key,value)
+	elseif type_val == 'jpg' then
+		return redis_api.just_cmd("ssdb_img_spare", key, "set", key,value)
+	else
+		return redis_api.just_cmd("ssdb_file_spare", key, "set", key,value)
+	end
+
+	--	if cacheTime then
+	--		return redis_api.hash_cmd("dfsdb_write_spare","expire",key,cacheTime)
+	--	end
 end 
 
 function set_to_redis_spare(key,value,cacheTime)
+	local st, ed, type_val = string.find(key, "%.(%a%a.)$")
 	if cacheTime then
-		return redis_api.hash_cmd("redis_write_spare", key, "setex",key,cacheTime,value)	
+		-- return redis_api.hash_cmd("redis_write_spare", key, "setex",key,cacheTime,value)	
+		--local st, ed, type_val = string.find(key, "%.(%a%a.)$")
+		if type_val == 'amr' then
+			return redis_api.hash_cmd("redis_amr_spare", key, "setex", key,cacheTime,value)
+		elseif type_val == 'mp4' then
+			return redis_api.hash_cmd("redis_mp4_spare", key, "setex", key,cacheTime,value)
+		elseif type_val == 'jpg' then
+			return redis_api.hash_cmd("redis_img_spare", key, "setex", key,cacheTime,value)
+		else
+			return redis_api.hash_cmd("redis_file_spare", key, "setex", key,cacheTime,value)
+		end
 	else
-		return redis_api.hash_cmd("redis_write_spare", key, "set",key,value)	
+		-- return redis_api.hash_cmd("redis_write_spare", key, "set",key,value)	
+		-- local st, ed, type_val = string.find(key, "%.(%a%a.)$")
+		if type_val == 'amr' then
+			return redis_api.hash_cmd("redis_amr_spare", key, "set", key,value)
+		elseif type_val == 'mp4' then
+			return redis_api.hash_cmd("redis_mp4_spare", key, "set", key,value)
+		elseif type_val == 'jpg' then
+			return redis_api.hash_cmd("redis_img_spare", key, "set", key,value)
+		else
+			return redis_api.hash_cmd("redis_file_spare", key, "set", key,value)
+		end
+
 	end
 end
- 
+
 function get_from_dfsdb_spare(key, group)
-	return redis_api.just_cmd("dfsdb_read_spare", group, "get", key)
+	-- return redis_api.just_cmd("dfsdb_read_spare", group, "get", key) -- change to ssdb
+	local st, ed, type_val = string.find(key, "%.(%a%a.)$")
+	if type_val == 'amr' then
+		return redis_api.just_cmd("ssdb_amr_spare", group, "get", key)
+	elseif type_val == 'mp4' then
+		return redis_api.just_cmd("ssdb_mp4_spare", group, "get", key)
+	elseif type_val == 'jpg' then
+		return redis_api.just_cmd("ssdb_img_spare", group, "get", key)
+	else
+		return redis_api.just_cmd("ssdb_file_spare", group, "get", key)
+	end
+
 end 
 
 function get_from_redis_spare(key, group)
-	return redis_api.just_cmd("redis_read_spare", group, "get", key)
+	-- return redis_api.just_cmd("redis_read_spare", group, "get", key)
+	local st, ed, type_val = string.find(key, "%.(%a%a.)$")
+	if type_val == 'amr' then
+		return redis_api.just_cmd("redis_amr_spare", group, "get", key)
+	elseif type_val == 'mp4' then
+		return redis_api.just_cmd("redis_mp4_spare", group, "get", key)
+	elseif type_val == 'jpg' then
+		return redis_api.just_cmd("redis_img_spare", group, "get", key)
+	else
+		return redis_api.just_cmd("redis_file_spare", group, "get", key)
+	end	
 end 
--- programs/RTAP/lua/dfsapi/deploy/link.lua
+++ programs/RTAP/lua/dfsapi/deploy/link.lua
@@ -1,147 +1,181 @@
 module("link")
 
 OWN_POOL = {
-	lhttp = {},
 	redis = {
 		-----------------------------------------------------------------------------------------------------
 		--dfsdb amr write port
 		dfsdb_amr_write = {
 			 hash = 'customer',
-                        {'M', 'dfsdb_1', '127.0.0.1', 7501, 30},
-                        {'M', 'dfsdb_2', '127.0.0.1', 7503, 30},
+                        {'M', 'dfsdb_1', '192.168.1.12', 7501, 30},
+                        {'M', 'dfsdb_2', '192.168.1.12', 7503, 30},
 		},
 		--dfsdb amr read port
 		dfsdb_amr_read = {
 			 hash = 'appoint',
-                        {'M', 'dfsdb_1', '127.0.0.1', 7501, 30},
-                        {'M', 'dfsdb_2', '127.0.0.1', 7503, 30},
-                        {'M', 'dfsdb_3', '127.0.0.1', 7506, 30},
-                        {'M', 'dfsdb_4', '127.0.0.1', 7508, 30},
+                        {'M', 'dfsdb_1', '192.168.1.12', 7502, 30},
+                        {'M', 'dfsdb_2', '192.168.1.12', 7504, 30},
 		},
 
 		--dfsdb mp4 write port 
 		dfsdb_mp4_write = {
 			 hash = 'customer',
-                        {'M', 'dfsdb_1', '127.0.0.1', 7501, 30},
-                        {'M', 'dfsdb_2', '127.0.0.1', 7503, 30},
+                        {'M', 'dfsdb_1', '192.168.1.12', 7501, 30},
+                        {'M', 'dfsdb_2', '192.168.1.12', 7503, 30},
 		},
 
 		--dfsdb mp4 read port
 		dfsdb_mp4_read = {	
 			 hash = 'appoint',
-                        {'M', 'dfsdb_1', '127.0.0.1', 7501, 30},
-                        {'M', 'dfsdb_2', '127.0.0.1', 7503, 30},
-                        {'M', 'dfsdb_3', '127.0.0.1', 7506, 30},
-                        {'M', 'dfsdb_4', '127.0.0.1', 7508, 30},
+                        {'M', 'dfsdb_1', '192.168.1.12', 7502, 30},
+                        {'M', 'dfsdb_2', '192.168.1.12', 7504, 30},
 		},
+		
 		
 		--dfsdb jpg write port 
 		dfsdb_jpg_write = {
 			 hash = 'customer',
-                        {'M', 'dfsdb_1', '127.0.0.1', 7501, 30},
-                        {'M', 'dfsdb_2', '127.0.0.1', 7503, 30},
+                        {'M', 'dfsdb_1', '192.168.1.12', 7501, 30},
+                        {'M', 'dfsdb_2', '192.168.1.12', 7503, 30},
 		},
 
 		--dfsdb jpg read port
 		dfsdb_jpg_read = {	
 			 hash = 'appoint',
-                        {'M', 'dfsdb_1', '127.0.0.1', 7501, 30},
-                        {'M', 'dfsdb_2', '127.0.0.1', 7503, 30},
-                        {'M', 'dfsdb_3', '127.0.0.1', 7506, 30},
-                        {'M', 'dfsdb_4', '127.0.0.1', 7508, 30},
+                        {'M', 'dfsdb_1', '192.168.1.12', 7501, 30},
+                        {'M', 'dfsdb_2', '192.168.1.12', 7503, 30},
 		},
 		
 		--dfsdb file write port
 		dfsdb_file_write = {
 			 hash = 'customer',
-                        {'M', 'dfsdb_1', '127.0.0.1', 7501, 30},
-                        {'M', 'dfsdb_2', '127.0.0.1', 7503, 30},
+                        {'M', 'dfsdb_1', '192.168.1.12', 7501, 30},
+                        {'M', 'dfsdb_2', '192.168.1.12', 7503, 30},
 		},
 		--dfsdb file read port
 		dfsdb_file_read = {
 			 hash = 'appoint',
-                        {'M', 'dfsdb_1', '127.0.0.1', 7501, 30},
-                        {'M', 'dfsdb_2', '127.0.0.1', 7503, 30},
-                        {'M', 'dfsdb_3', '127.0.0.1', 7506, 30},
-                        {'M', 'dfsdb_4', '127.0.0.1', 7508, 30},
+                        {'M', 'dfsdb_1', '192.168.1.12', 7502, 30},
+                        {'M', 'dfsdb_2', '192.168.1.12', 7504, 30},
 		},
 
 		--dfsdb write spare port
 		dfsdb_write_spare = {
-			hash = 'customer',
-			{'M', 'dfsdb_bak', '127.0.0.1', 7501, 30},
+			 hash = 'customer',
+                        {'M', 'dfsdb_bak', '192.168.1.12', 7501, 30},
 		},
 		--dfsdb read spare port
 		dfsdb_read_spare = {
 			hash = 'appoint',
-			{'M', 'dfsdb_bak', '127.0.0.1', 7501, 30},
+                        {'M', 'dfsdb_bak', '192.168.1.12', 7502, 30}, 
+		},
+		
+		-- ssdb file spare port
+		ssdb_file_spare = {
+			 hash = 'customer',
+                        {'M', 'ssdb_bak', '192.168.71.84', 8890, 30},
+		},
+ 		-- ssdb mp4 spare port
+		ssdb_mp4_spare = {
+			 hash = 'customer',
+                        {'M', 'ssdb_bak', '192.168.71.84', 8891, 30},
+		},
+		-- ssdb amr spare port
+		ssdb_amr_spare = {
+			 hash = 'customer',
+                        {'M', 'ssdb_bak', '192.168.71.84', 8892, 30},
+		},
+		-- ssdb img spare port
+		ssdb_img_spare = {
+			 hash = 'customer',
+                        {'M', 'ssdb_bak', '192.168.71.84', 8893, 30},
 		},
 	
-
 		--redis amr write port
 		redis_amr_write = {
 			 hash = 'customer',
-                        {'M', 'redis_1', '127.0.0.1', 7502, 30},
-                        {'M', 'redis_2', '127.0.0.1', 7504, 30},
+                        {'M', 'redis_1', '192.168.1.12', 7505, 30},
+                        {'M', 'redis_2', '192.168.1.12', 7507, 30},
 		},
 		--redis amr read port
 		redis_amr_read = {
 			 hash = 'appoint',
-                        {'M', 'redis_1', '127.0.0.1', 7502, 30},
-                        {'M', 'redis_2', '127.0.0.1', 7504, 30},
+                        {'M', 'redis_1', '192.168.1.12', 7505, 30},
+                        {'M', 'redis_2', '192.168.1.12', 7507, 30},
 		},
 
 		--redis mp4 write port 
 		redis_mp4_write = {
 			 hash = 'customer',
-                        {'M', 'redis_1', '127.0.0.1', 7502, 30},
-                        {'M', 'redis_2', '127.0.0.1', 7504, 30},
+                        {'M', 'redis_1', '192.168.1.12', 7505, 30},
+                        {'M', 'redis_2', '192.168.1.12', 7507, 30},
 		},
 
 		--redis mp4 read port
 		redis_mp4_read = {	
 			 hash = 'appoint',
-                        {'M', 'redis_1', '127.0.0.1', 7502, 30},
-                        {'M', 'redis_2', '127.0.0.1', 7504, 30},
+                        {'M', 'redis_1', '192.168.1.12', 7505, 30},
+                        {'M', 'redis_2', '192.168.1.12', 7507, 30},
 		},
 		
-		--redis jpg write port 
+		--dfsdb jpg write port 
 		redis_jpg_write = {
 			 hash = 'customer',
-                        {'M', 'redis_1', '127.0.0.1', 7502, 30},
-                        {'M', 'redis_2', '127.0.0.1', 7504, 30},
+                        {'M', 'redis_1', '192.168.1.12', 7505, 30},
+                        {'M', 'redis_2', '192.168.1.12', 7507, 30},
 		},
 
-		--redis jpg read port
+		--dfsdb jpg read port
 		redis_jpg_read = {	
 			 hash = 'appoint',
-                        {'M', 'redis_1', '127.0.0.1', 7502, 30},
-                        {'M', 'redis_2', '127.0.0.1', 7504, 30},
+                        {'M', 'redis_1', '192.168.1.12', 7505, 30},
+                        {'M', 'redis_2', '192.168.1.12', 7507, 30},
 		},
 		
-		--redis file write port
+		--dfsdb file write port
 		redis_file_write = {
 			 hash = 'customer',
-                        {'M', 'redis_1', '127.0.0.1', 7502, 30},
-                        {'M', 'redis_2', '127.0.0.1', 7504, 30},
+                        {'M', 'redis_1', '192.168.1.12', 7505, 30},
+                        {'M', 'redis_2', '192.168.1.12', 7507, 30},
 		},
-		--redis file read port
+		--dfsdb file read port
 		redis_file_read = {
 			 hash = 'appoint',
-                        {'M', 'redis_1', '127.0.0.1', 7502, 30},
-                        {'M', 'redis_2', '127.0.0.1', 7504, 30},
+                        {'M', 'redis_1', '192.168.1.12', 7505, 30},
+                        {'M', 'redis_2', '192.168.1.12', 7507, 30},
 		},
 
 		--dfsdb write spare port
 		redis_write_spare = {
-			hash = 'customer',
-			{'M', 'redis_bak', '127.0.0.1', 7502, 30},
+			 hash = 'customer',
+                        {'M', 'redis_bak', '192.168.1.12', 7505, 30},
 		},
 		--dfsdb read spare port
 		redis_read_spare = {
-			hash = 'appoint',
-			{'M', 'redis_bak', '127.0.0.1', 7502, 30},
+			 hash = 'appoint',
+                        {'M', 'redis_bak', '192.168.1.12', 7505, 30},
 		},
+
+                -- redis spare file,amr,mp4,jpg
+		redis_file_spare = {
+			hash = 'customer',
+	       		{'M', 'redis_bak', '192.168.1.12', 7505, 30},
+      		},
+		redis_amr_spare = {
+			 hash = 'customer',
+                        {'M', 'redis_bak', '192.168.1.12', 7505, 30},
+		},
+		redis_mp4_spare = {
+			 hash = 'customer',
+                        {'M', 'redis_bak', '192.168.1.12', 7505, 30},
+		},
+		redis_img_spare = {
+			 hash = 'customer',
+                        {'M', 'redis_bak', '192.168.1.12', 7505, 30},
+		},
+	
+		
+			
+	
 		----------------------------------------------------------------------------------------------------------
 --		tmpamr = {
 --			host = '172.16.31.186',
@@ -164,16 +198,36 @@ OWN_POOL = {
 OWN_DIED = {
 	redis = {
 		private = {
-			host = '127.0.0.1',
+			host = '192.168.1.12',
 			port = 6379,
 		},
 		public = {
-			host = '127.0.0.1',
+			host = '192.168.1.12',
+			port = 6379,
+		},
+		tmpamr = {
+			host = '192.168.1.12',
+			port = 6379,
+		},
+		tmpvideo = {
+			host = '192.168.1.12',
+			port = 6379,
+		},
+		tmpimage = {
+			host = '192.168.1.12',
+			port = 6379,
+		},
+		redis_bak = {
+			host = '192.168.1.12',
 			port = 6379,
 		},
 	},
 	mysql = {},
 	http = {
+		jTTSServer = {
+			host = "192.168.1.9",
+			port = 3000,
+		},
 		["dfsapi/v2/mp32voice"] = {
 			host = "192.168.1.207",
 			--host = "192.168.1.6",
