6385028f9c079ea61e31b6c973aa67a744b39407
guoguanghua <guoguanghua@mirrtalk.com>
2016-04-09 14:04:02 +0800

    Added fd_array & fd_list and implements it.

--- programs/CoreExchangeNode/fd_manager.h
+++ programs/CoreExchangeNode/fd_manager.h
@@ -15,24 +15,59 @@
  *      ...               ...               ...
  * */
 
+/*
+  构建的数据结构需要做到两点快速访问。
+  1, 根据fd, 能找到对应fd 的相关状态以及fd所相关的一切描述。
+  2, 根据， 相关描述快速定位到相关fd, 
+*/
+
 struct fd_node;
 
 struct fd_node {
-	int fd;
-	uint8_t status; // Descriptor fd connect status.
-        struct fd_node *next;
+  int fd;
+  uint8_t status; // Descriptor fd connect status.
+  struct fd_node *next;
 };
 
 struct fd_head {
-        enum router_object obj;
-	uint32_t size;
-	struct fd_node *node;
+  enum router_object obj;
+  uint32_t size;
+  struct fd_node *next;
 };
 
 struct fd_list {
-        struct fd_head [FD_MAX_CLASSIFICATION];
+  struct fd_head [FD_MAX_CLASSIFICATION];
 };
+
+int list_init();
+int list_destroy();
+int list_remove(const enum router_object obj, const int fd);
+int list_push_back(const enum router_object obj,
+		   const struct fd_node &node);
+int list_front(const enum router_object obj,
+	       struct fd_node &node);
+
+
+struct fd_descriptor {
+  uint32_t ip;
+  uint16_t port;
+  uint8_t status; // 0 未使用， 1 连接正常， 2 已经closed掉。
+  enum router_object obj;
+};
+
+struct fd_array {
+  struct fd_descriptor *dsp_array;
+  int max_fd;
+  uint32_t cap;
+};
+
+int array_init();
+int array_destroy();
+int array_fill_fd(const int fd, const struct fd_descriptor &des);
+int array_remove_fd(const int fd);
+int array_at_fd(const int fd, struct fd_descriptor &des);
 
 int find_router_object(enum router_object obj);
 int append_router_object(enum router_object obj, int fd);
+
 #endif
--- programs/CoreExchangeNode/message_dispatch.c
+++ programs/CoreExchangeNode/message_dispatch.c
@@ -8,19 +8,19 @@
 extern uint32_t get_local_ip();
 void cid_compose(uint32_t ip, int fd, struct CID *cid)
 {
-	    cid = (struct CID*) malloc(sizeof(struct CID));
-        cid->IP = ip;
-        cid->fd = fd;
+  cid = (struct CID*) malloc(sizeof(struct CID));
+  cid->IP = ip;
+  cid->fd = fd;
 }
 
 static void client_msg_pack(struct message *ready_msg, struct message *org_msg)
 {
-	    ready_msg->message_head.message_from = ROUTER_SERVER;
-        ready_msg->message_head.message_to = org_msg->message_head.message_to;  
-        ready_msg->message_head.CID_number = 1;
-		ready_msg->message_head.cid = (struct CID**) malloc(sizeof(struct CID*));
-	    cid_compose(get_local_ip(), org_msg->message_fd,
-					ready_msg->message_head.cid[0]);
+  ready_msg->message_head.message_from = ROUTER_SERVER;
+  ready_msg->message_head.message_to = org_msg->message_head.message_to;  
+  ready_msg->message_head.CID_number = 1;
+  ready_msg->message_head.cid = (struct CID**) malloc(sizeof(struct CID*));
+  cid_compose(get_local_ip(), org_msg->message_fd,
+              ready_msg->message_head.cid[0]);
 }
 
 static void gateway_msg_pack(struct message *ready_msg, struct message *org_msg)
@@ -30,10 +30,10 @@ static void gateway_msg_pack(struct message *ready_msg, struct message *org_msg)
 
 static void client_route_to_gateway(struct message *msg)
 {
-	// 从客户端来的消息需要路由去多台gateway, 还是寻找最好的gateway路由。
-        int fd = find_router_object(MESSAGE_GATEWAY);
-		msg->message_fd = fd;
-		push_message(msg);
+  // 从客户端来的消息需要路由去多台gateway, 还是寻找最好的gateway路由。
+  int fd = find_router_object(MESSAGE_GATEWAY);
+  msg->message_fd = fd;
+  push_message(msg);
 }
 
 static void gateway_route_to_client(struct message *msg)
@@ -43,37 +43,37 @@ static void gateway_route_to_client(struct message *msg)
 
 static void router_select(struct message *msg, enum router_object obj)
 {
-        switch (obj) {
-        case CLIENT:
-		  client_route_to_gateway(msg);
-		  break;
-	    case MESSAGE_GATEWAY:
-		  gateway_route_to_client(msg);
-		  break;
-		case ROUTER_SERVER:
-		  break;
-	    default:
-		  error("msg data() error, can't find message to server:%d .", obj);
-	    }
+  switch (obj) {
+  case CLIENT:
+    client_route_to_gateway(msg);
+    break;
+  case MESSAGE_GATEWAY:
+    gateway_route_to_client(msg);
+    break;
+  case ROUTER_SERVER:
+    break;
+  default:
+    error("msg data() error, can't find message to server:%d .", obj);
+  }
 }
 
 void message_dispatch()
 {
-        struct message* org_msg = (struct message*)malloc(sizeof(struct message));
-        struct message* ready_msg = (struct message*)malloc(sizeof(struct message));
-        pop_message(org_msg);
-        switch (org_msg->message_head.message_from) {
-        case CLIENT:
-	      client_msg_pack(ready_msg, org_msg);
-	      break;
-        case MESSAGE_GATEWAY:
-	      gateway_msg_pack(ready_msg, org_msg);
-	    break;
-        default:
-	      error("msg head data (message_from :%d) is not right.",
-			    org_msg->message_head.message_from);
-        }
-        router_select(ready_msg, ready_msg->message_head.message_to);
-		free(org_msg);
-		free(ready_msg);
+  struct message* org_msg = (struct message*)malloc(sizeof(struct message));
+  struct message* ready_msg = (struct message*)malloc(sizeof(struct message));
+  pop_message(org_msg);
+  switch (org_msg->message_head.message_from) {
+  case CLIENT:
+    client_msg_pack(ready_msg, org_msg);
+    break;
+  case MESSAGE_GATEWAY:
+    gateway_msg_pack(ready_msg, org_msg);
+    break;
+  default:
+    error("msg head data (message_from :%d) is not right.",
+          org_msg->message_head.message_from);
+  }
+  router_select(ready_msg, ready_msg->message_head.message_to);
+  free(org_msg);
+  free(ready_msg);
 }
