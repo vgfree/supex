733473913db0aee39b5eeaf878ed96888ddaf73c
chenliuliu <chenliuliu@mirrtalk.com>
2016-04-11 12:02:49 +0800

    new pack_and_parse

---
+++ lib/libcomm/new_pack_parse/pack.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct mfptp_frame
+{
+   int   size;
+   char  *frame_buf;
+   struct mfptp_frame * next;
+};
+
+struct mfptp_pack
+{
+   short         version;
+   short         sub_version;
+   unsigned char compress;
+   unsigned char encrypt;
+   unsigned char method;
+   unsigned char packages;
+   char          *pack_buf;
+   int           pack_buf_size;
+   struct mfptp_frame *m_frame;
+   struct mfptp_pack  *next;
+};
+
+
+
+int  mfptp_pack_len(int len);
+
+struct mfptp_pack *copy_list(struct mfptp_pack *head,struct mfptp_pack *cop,int len);
+
+struct mfptp_frame *copy_frame_list(struct mfptp_frame *head,struct mfptp_frame *cop,int len);
+
+struct mfptp_pack * pack_one_package(struct mfptp_pack * p_info);
+
+struct mfptp_pack *  mfptp_packages(struct mfptp_pack * p_info);
