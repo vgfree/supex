4992569cd55921aa617e3622b83b3cc405c8a7f0
baoxue <baoxue@daoke.me>
2016-04-25 19:54:30 +0800

    fixme

--- programs/transit-pic/api_pic_newstatus.lua
+++ programs/transit-pic/api_pic_newstatus.lua
@@ -1,44 +1,39 @@
 local utils     = require('utils')
 local only      = require('only')
 local cjson     = require('cjson')
-local socket    = require('socket')
-local mysql_api = require('mysql_pool_api')
 local redis_api = require('redis_pool_api')
 local http_api  = require('http_short_api')
 
 module("api_pic_newstatus", package.seeall)
 
 function dfs_image_save( str )
-  --Send picture to dfs server
-  local dfsServer = link["OWN_DIED"]["http"]["dfsapi/v2/saveImage"]
+	--Send picture to dfs server
+	local dfsServer = link["OWN_DIED"]["http"]["dfsapi/v2/saveImage"]
 
-  local file = {
-    file_name = 'ClayMore.jpg',
-    data = str,
-    data_type = 'application/octet-stream',
-   }
+	local file = {
+		file_name = 'ClayMore.jpg',
+		data = str,
+		data_type = 'application/octet-stream',
+	}
 
-  local tab = {
-    appKey = '2290837278',
-    length = tostring(#str),
-    sign = '711096F76AD42DD3E5E16B1657626AF8A385E7CD',
-    isStorage = 'true',
-    file_name = 'ClayMore.jpg',
-    data = str,
-  }
+	local tab = {
+		appKey = '2290837278',
+		length = tostring(#str),
+		sign = '711096F76AD42DD3E5E16B1657626AF8A385E7CD',
+		isStorage = 'true',
+		file_name = 'ClayMore.jpg',
+		data = str,
+	}
 
-  local req_data = utils.compose_http_form_request(dfsServer, 'dfsapi/v2/saveImage', nil, tab, "mmfile", file)
-  local ret = http_api.http(dfsServer, req_data , true)
-  if not ret then
-    print("[FAILED] HTTP post failed! feedback api")
-  end
-  print("ret = %s", ret)
-  return ret
+	local req_data = utils.compose_http_form_request(dfsServer, 'dfsapi/v2/saveImage', nil, tab, "mmfile", file)
+	local ret = http_api.http(dfsServer, req_data , true)
+	if not ret then
+		print("[FAILED] HTTP post failed! feedback api")
+	end
+	print("ret = %s", ret)
+	return ret
 end
 
 function handle( str )
-  str = string.gsub(str,"Head=picture&data=","")
-  local ret = dfs_image_save(str)
-  return ret
-  
+	return dfs_image_save(str)
 end
--- programs/transit-pic/test.lua
+++ programs/transit-pic/test.lua
@@ -1,28 +1,35 @@
 local path_list ={
-	"lua/core/?.lua;",
-	"lua/code/?.lua;",
+        "lua/core/?.lua;",
+        "lua/code/?.lua;",
 
 
-	"../../open/lib/?.lua;",
-	"../../open/apply/?.lua;",
-	"../../open/spxonly/?.lua;",
-	"../../open/linkup/?.lua;",
-	"../../open/public/?.lua;",
+        "../../open/lib/?.lua;",
+        "../../open/apply/?.lua;",
+        "../../open/spxonly/?.lua;",
+        "../../open/linkup/?.lua;",
+        "../../open/public/?.lua;",
 
-	"open/?.lua;",
+        "open/?.lua;",
 }
 
 package.path = table.concat(path_list) .. package.path
 package.cpath = "../../open/lib/?.so;" .. "open/?.so;" .. package.cpath
-
 require("zmq")
 
-local redis_api = require('redis_pool_api')
-
-redis_api.init( )
-
-local api_dk_newstatus = require("api_dk_newstatus")
+local ctx = zmq.init(1)
+local s = ctx:socket(zmq.PUSH)
 
 
-local data = 'keyvalue\r\nnt=0&mt=21776&Accountid=111111111111111&gps=221215102957,11444.98295E,4045.92141N,262,18,746;221215102958,11444.97898E,4045.92095N,262,19,746;221215102959,11444.97479E,4045.92069N,263,20,746;221215103000,11444.97046E,4045.92029N,262,20,746;221215103001,11444.96605E,4045.91999N,262,21,746&tokencode=0al15UMqpo&imsi=460022211427272&imei=752795632561713&mod=SG900'
-api_dk_newstatus.handle( data )
+s:bind("tcp://*:5556")
+
+
+local fd  = io.open("Craire.jpeg", "r")
+local pic = fd:read("*a")
+fd:close()
+
+local picStr = string.format("%s", pic)
+s:send(picStr)
+
+s:close()
+print("S has closed")
+ctx:term()
