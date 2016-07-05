bade2898b42b5423d3a2ea6ce46618584518c6d9
lanjian <lanjian@mirrtalk.com>
2016-07-05 19:52:46 +0800

    update bind client

---
+++ programs/CoreExchangeNode/bind_client/core_exchange_node_test.h
@@ -0,0 +1,16 @@
+#ifndef _CORE_EXCHANGE_NODE_TEST_H
+#define _CORE_EXCHANGE_NODE_TEST_H
+
+#include "router.h"
+
+#include <stdlib.h>
+
+int test_uid_map();
+
+int test_gid_map();
+
+int test_simulate_client(char *ip);
+
+int test_simulate_gateway();
+#endif
+
