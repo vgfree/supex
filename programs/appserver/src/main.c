4eec224288fd75f494675d93244ba431aec306fd
tianlu <tianlu@mirrtalk.com>
2016-07-06 11:04:56 +0800

    添加线程池

--- programs/appserver/src/main.c
+++ programs/appserver/src/main.c
@@ -1,9 +1,9 @@
 /*
-* CopyRight    : DT+
-* Author       : louis.tin
-* Date         : 06-28-2016
-* Description  : Appserver
-*/
+ * CopyRight    : DT+
+ * Author       : louis.tin
+ * Date         : 06-28-2016
+ * Description  : Appserver
+ */
 
 #include <string.h>
 #include <stdlib.h>
@@ -23,51 +23,34 @@
 #include "appsrv.h"
 #include "json.h"
 
+#include "core/evcs_module.h"
+#include "core/evcs_events.h"
+#include "core/evcs_kernel.h"
+#include "spx_evcs.h"
+#include "spx_evcs_module.h"
+#include "async_tasks/async_obj.h"
+#include "base/free_queue.h"
+//#include "dispatch_data.h"
+
+EVCS_MODULE_SETUP(kernel, kernel_init, kernel_exit, &g_kernel_evts);
+EVCS_MODULE_SETUP(evcs, evcs_init, evcs_exit, &g_evcs_evts);
+#include "_ev_coro.h"
+#include "evcoro_async_tasks.h"
+#include "thread_pool_loop/tlpool.h"
+
+
+#define MAX_PTHREAD_COUNT 3
+#define MAX_UTHREAD_COUNT 5
+
 struct smart_cfg_list g_smart_cfg_list = {};
 
-char load_cid[256] = {};
+//struct app_msg recv_msg = {};
 
-static void stackDump (lua_State *L);
-static int get_uid(lua_State *L);
-static int set_uidmap(lua_State *L);
-static int send_msg(lua_State *L);
-int luaopen_power(lua_State *L);
-void *t_msg_recv();
-int smart_vms_call_set(void *user, union virtual_system **VMS, struct adopt_task_node *task);
-
-int main(int argc, char **argv)
+struct user_task
 {
-	create_io();
-
-	pthread_t tid_recv;
-	int ret = 0;
-	ret = pthread_create(&tid_recv, NULL, (void *)t_msg_recv, NULL);
-	if (ret != 0) {
-		printf("Create send pthread error!\n");
-		return -1;
-	}
-
-	load_supex_args(&g_smart_cfg_list.argv_info, argc, argv, NULL, NULL, NULL);
-
-	load_cfg_file(&g_smart_cfg_list.file_info, g_smart_cfg_list.argv_info.conf_name);
-
-	g_smart_cfg_list.func_info[SET_FUNC_ORDER].type = BIT8_TASK_TYPE_ALONE;
-	g_smart_cfg_list.func_info[SET_FUNC_ORDER].func = (TASK_VMS_FCB)smart_vms_call_set;
-
-	smart_mount(&g_smart_cfg_list);
-	smart_start();
-
-	return 0;
-}
-
-int luaopen_power(lua_State *L)
-{
-	lua_register(L, "get_uid", get_uid);
-	lua_register(L, "set_uidmap", set_uidmap);
-	lua_register(L, "send_msg",  send_msg);
-
-	return 0;
-}
+        char    *user;
+        struct app_msg recv_msg;
+};
 
 static int get_uid(lua_State *L)
 {
@@ -182,128 +165,217 @@ static int send_msg(lua_State *L)
 
 }
 
-void *t_msg_recv()
+lua_State *lua_vm_init(void)
 {
+	lua_State *L;
+	int error = 0;
+	L = luaL_newstate();
+	assert(L);
+	luaopen_base(L);
+	luaL_openlibs(L);
+
+	lua_register(L, "get_uid", get_uid);
+	lua_register(L, "set_uidmap", set_uidmap);
+	lua_register(L, "send_msg",  send_msg);
+
+
+	error = luaL_dofile(L, "lua/core/init.lua");
+	if (error) {
+		fprintf(stderr, "%s\n", lua_tostring(L, -1));
+		lua_pop(L, 1);
+		exit(EXIT_FAILURE);
+	}
+
+	error = luaL_dofile(L, "lua/core/start.lua");
+
+	if (error) {
+		//x_printf(E, "%s\n", lua_tostring(L, -1));
+		lua_pop(L, 1);
+		exit(EXIT_FAILURE);
+	}
+	return L;
+}
+
+// 监视是否有数据
+static bool task_lookup(void *user, void *task)
+{
+	tlpool_t *pool = user;
+	bool ok = false;
+	int idx = tlpool_get_thread_index(pool);
+	// 抢占模式
+	ok = tlpool_pull(pool, task, TLPOOL_TASK_SEIZE, idx);
+
+	if (!ok) {
+		return -1;
+	}
+
+	return ok;
+}
+
+void *task_handle(struct supex_evcoro *evcoro, int step)
+{
+	lua_State *L = NULL;
+	// 将数据取出
+	struct user_task *p_task = &((struct user_task *)evcoro->task)[step];
+	union virtual_system *p_VMS = &((union virtual_system *)evcoro->VMS)[step];
+	// 判断Lua虚拟机是否存在, 存在则赋给L, 不存在则初始化
+	if (p_VMS->L) {
+		L = p_VMS->L;
+	} else {
+		L = lua_vm_init();
+		evcoro->VMS[step].L = L;
+	}
+
+	if (!L) {
+		printf("lua vm is NULL\n");
+		free(p_task->user);
+		exit(0);
+	}
+
+	/******************************************/
+
+	// 执行数据处理
+//	lua_State *L;
+//	L = luaL_newstate(); // 打开lua
+	luaL_openlibs(L); // 打开标准库
+
+//	luaopen_power(L);
+
+	int status = luaL_loadfile(L, "script.lua");
+	if (status) {
+		perror("luaL_dofile error");
+		exit(1);
+	}
+
+	lua_newtable(L);
+	int i;
+	for (i = 1; i <= recv_msg.vector_size; i++) {
+		printf("recv_msg size:%d\t [%d]iov_len:%d\t [%d]iov_base = %s,\n",
+				recv_msg.vector_size, i - 1, recv_msg.vector[i - 1].iov_len, i -1 , recv_msg.vector[i - 1].iov_base);
+		lua_pushnumber(L, i);
+		lua_pushlstring(L, recv_msg.vector[i - 1].iov_base, recv_msg.vector[i - 1].iov_len);
+		lua_rawset(L, -3);
+	}
+
+	lua_setglobal(L, "msg");
+
+	int result = lua_pcall(L, 0, LUA_MULTRET, 0);
+	if (result) {
+		fprintf(stdout, "bad, bad script\n");
+		exit(1);
+	}    /* 获得堆栈顶的值*/
+	int sum = lua_tonumber(L, lua_gettop(L));
+	if (!sum) {
+		fprintf(stdout, "lua_tonumber() failed!\n");
+		exit(1);
+	}
+	fprintf(stdout, "Script returned: %d\n", sum);
+	lua_pop(L, 1);
+	printf("top = %d\n", lua_gettop(L));
+	lua_close(L);
+
+
+//	dispatch_data(L, p_task->user, strlen(p_task->user), g_user_key, strlen(g_user_key), p_task->redis_cnt);
+	free(p_task->user);
+}
+
+void task_worker(void *data)
+{
+	/*load module*/
+	EVCS_MODULE_MOUNT(kernel);
+	EVCS_MODULE_ENTRY(kernel, true);
+
+	struct evcs_argv_settings sets = {
+		.num    = MAX_UTHREAD_COUNT     /*协程数*/
+			, .tsz  = sizeof(struct app_msg)
+			, .data = data
+			, .task_lookup = task_lookup
+			, .task_handle = task_handle
+	};
+	EVCS_MODULE_CARRY(evcs, &sets);
+	EVCS_MODULE_MOUNT(evcs);
+	EVCS_MODULE_ENTRY(evcs, true);
+
+	EVCS_MODULE_START();
+}
+
+static bool task_report(void *user, void *task)
+{
+        tlpool_t        *pool = user;
+        bool            ok = false;
+
+        ok = tlpool_push(pool, task, TLPOOL_TASK_SEIZE, 0);
+        return ok;
+}
+
+int main(int argc, char **argv)
+{
+	create_io();
+	// 1. 初始化线程池
+	tlpool_t *tlpool = tlpool_init(MAX_PTHREAD_COUNT, 100, sizeof(struct app_msg), NULL);
+	int idx;
+	for (idx = 0; idx < MAX_PTHREAD_COUNT; idx++) {
+		tlpool_bind(tlpool, (void (*)(void *))task_worker, tlpool, idx);
+	}
+	tlpool_boot(tlpool);
+
+	EVCS_MODULE_MOUNT(kernel);
+	EVCS_MODULE_ENTRY(kernel, true);
+
+	// 2. 循环接收数据
+	struct app_msg recv_msg = {};
+	int more = 0;
+//	struct user_task task;
 
 	while (1) {
-		struct app_msg recv_msg = {};
-		int more = 0;
-		recv_app_msg(&recv_msg, &more, -1);
-		int i;
-		for (i = 0; i <= recv_msg.vector_size; i++) {
-			printf("recv_msg size:%d\t [%d]iov_len:%d\t [%d]iov_base = %s,\n",
-					recv_msg.vector_size, i, recv_msg.vector[i].iov_len, i , recv_msg.vector[i].iov_base);
-		}
-
-		char str[32];
-		strcpy(str, recv_msg.vector[0].iov_base);
-		int len = strlen(str);
-
-		printf("len = %d\t第一帧 = %s\n", len, str);
-
-		{
-			lua_State *L;
-			L = luaL_newstate(); // 打开lua
-			luaL_openlibs(L); // 打开标准库
-
-			luaopen_power(L);
-
-			int status = luaL_loadfile(L, "script.lua");
-			if (status) {
-				perror("luaL_dofile error");
-				exit(1);
-			}
-
-			lua_newtable(L);
-
-			for (i = 1; i <= recv_msg.vector_size; i++) {
-				printf("recv_msg size:%d\t [%d]iov_len:%d\t [%d]iov_base = %s,\n",
-						recv_msg.vector_size, i - 1, recv_msg.vector[i - 1].iov_len, i -1 , recv_msg.vector[i - 1].iov_base);
-				lua_pushnumber(L, i);
-				lua_pushlstring(L, recv_msg.vector[i - 1].iov_base, recv_msg.vector[i - 1].iov_len);
-				lua_rawset(L, -3);
-			}
-			printf("\n");
-			// 声明lua中的变量
-			lua_setglobal(L, "msg");
-			//lua_call(L, 0, 0);
-			int result = lua_pcall(L, 0, LUA_MULTRET, 0);
-			if (result) {
-				fprintf(stdout, "bad, bad script\n");
-				exit(1);
-			}    /* 获得堆栈顶的值*/
-			int sum = lua_tonumber(L, lua_gettop(L));
-			if (!sum) {
-				fprintf(stdout, "lua_tonumber() failed!\n");
-				exit(1);
-			}
-			fprintf(stdout, "Script returned: %d\n", sum);
-			lua_pop(L, 1);
-			printf("top = %d\n", lua_gettop(L));
-			lua_close(L);
-			printf("\n");
+		if (recv_app_msg(&recv_msg, &more, -1)) {
+			task_report(tlpool, &recv_msg);
+			printf("在这里\n");
 		}
 	}
+#if 0
+	/*******************************************/
+	// 执行数据处理
+	lua_State *L;
+	L = luaL_newstate(); // 打开lua
+	luaL_openlibs(L); // 打开标准库
+
+//	luaopen_power(L);
+
+	int status = luaL_loadfile(L, "script.lua");
+	if (status) {
+		perror("luaL_dofile error");
+		exit(1);
+	}
+
+	lua_newtable(L);
+	int i;
+	for (i = 1; i <= recv_msg.vector_size; i++) {
+		printf("recv_msg size:%d\t [%d]iov_len:%d\t [%d]iov_base = %s,\n",
+				recv_msg.vector_size, i - 1, recv_msg.vector[i - 1].iov_len, i -1 , recv_msg.vector[i - 1].iov_base);
+		lua_pushnumber(L, i);
+		lua_pushlstring(L, recv_msg.vector[i - 1].iov_base, recv_msg.vector[i - 1].iov_len);
+		lua_rawset(L, -3);
+	}
+
+	lua_setglobal(L, "msg");
+
+	int result = lua_pcall(L, 0, LUA_MULTRET, 0);
+	if (result) {
+		fprintf(stdout, "bad, bad script\n");
+		exit(1);
+	}    /* 获得堆栈顶的值*/
+	int sum = lua_tonumber(L, lua_gettop(L));
+	if (!sum) {
+		fprintf(stdout, "lua_tonumber() failed!\n");
+		exit(1);
+	}
+	fprintf(stdout, "Script returned: %d\n", sum);
+	lua_pop(L, 1);
+	printf("top = %d\n", lua_gettop(L));
+	lua_close(L);
+#endif
 }
-
-int smart_vms_call_set(void *user, union virtual_system **VMS, struct adopt_task_node *task)
-{
-	char msg[64];
-
-	struct data_node *p_node = get_pool_data(task->sfd);
-	char *p_buf = cache_data_address(&p_node->mdl_recv.cache);
-	printf("p_buf = %s\n", p_buf);
-
-	struct redis_status *p_rst = &p_node->mdl_recv.parse.redis_info.rs;
-
-	printf("len: %d \n", p_rst->field[1].len);
-	printf("command_type: %x \n", p_rst->command_type);
-
-	memcpy(msg, p_buf + p_rst->field[1].offset, MIN(p_rst->field[1].len, 64 - 1));
-	struct app_msg  send_msg = {};
-	send_app_msg(&send_msg);
-
-	const char sndcnt[] = ":1\r\n";
-	cache_append(&p_node->mdl_send.cache, sndcnt, sizeof(sndcnt) - 1);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /*
