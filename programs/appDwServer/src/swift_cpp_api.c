f27fb13a253f9275d55d85111927918edfdbfc3d
baoxue <459032139@qq.com>
2016-07-07 14:02:41 +0800

    fix appDwServer

--- programs/appDwServer/src/swift_cpp_api.c
+++ programs/appDwServer/src/swift_cpp_api.c
@@ -3,6 +3,14 @@
 #include "major/swift_api.h"
 #include "swift_cpp_api.h"
 
+char *memdup(char *src, int size)
+{
+	char *new = calloc(1, size);
+	assert(new);
+	memcpy(new, src, size);
+	return new;
+}
+
 int swift_vms_call(void *user, union virtual_system **VMS, struct adopt_task_node *task)
 {
 	tlpool_t                *tlpool = user;
@@ -13,14 +21,14 @@ int swift_vms_call(void *user, union virtual_system **VMS, struct adopt_task_nod
 	struct redis_status     *p_rst = &p_node->mdl_recv.parse.redis_info.rs;
 
 
-
-	sniff_task.size = MIN((int)p_rst->field[1].len, MAX_SNIFF_FLOWS_LENGTH);
-
-	if (sniff_task.size != p_rst->field[1].len) {
-		x_printf(E, "send size:%d real size:%zd", sniff_task.size, p_rst->field[1].len);
+	int i;
+	struct app_msg  send_msg = {0};
+	send_msg.vector_size = p_rst->fields;
+	for (i = 0; i < p_rst->fields; i++) {
+		send_msg.vector[i].iov_base = memdup(p_buf + p_rst->field[i].offset, p_rst->field[i].len);
+		send_msg.vector[i].iov_len = p_rst->field[i].len;
 	}
-
-	memcpy(p_rmsg->flows, p_buf + p_rst->field[1].offset, sniff_task.size);
+	send_app_msg(&send_msg);
 
 
 	const char sndcnt[] = ":1\r\n";
