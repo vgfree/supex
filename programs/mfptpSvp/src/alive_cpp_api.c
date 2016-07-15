6dbb5e3aaa2b55a216ce2f56c6af1a406ca25603
guoguanghua <guoguanghua@mirrtalk.com>
2016-07-15 19:28:16 +0800

    续传数据为代码

--- programs/mfptpSvp/src/alive_cpp_api.c
+++ programs/mfptpSvp/src/alive_cpp_api.c
@@ -44,16 +44,45 @@ int alive_vms_call(void *user, union virtual_system **VMS, struct adopt_task_nod
 
 	alive_send_data(true, task->cid, task->sfd, "good", 5);
 
+// start
+	key = // 从客户端接收的数据提取key.
+	fd = hkey_get_fd(key); // 通过接收到的key 寻找cid.
+	if (fd != -1) // 如该cid 存在， 那么可能之前有数据存储.
+	{
+		struct residue_package package;
+		char *value = hkey_get_value(key); //从对应的libkv 中取得之前存的package.
+		int offset = hkey_get_offset(key); //数据的偏移.
+		init_residue_package(&package, task->cid.fd, offset, value, strlen(value)); // 不支持二进制.
+		push_residue_package(&package);// 把package 重新存到io thread 的buffer.	
+		hkey_del_fd(key); // 把之前存储的所有对应内容清空.
+		hkey_del_value(key);
+		hkey_del_fd_key(fd);
+		destroy_residue_package(&package);
+	}
+	else {
+		//如果不存在，那么是第一次握手，不用处理.
+	}
+	hkey_insert_fd(key, cid.fd); // 存key, cid. 更新对应的key 与cid 的bind.
+// end
 	return 0;
 }
 
 int alive_vms_online(void *user, union virtual_system **VMS, struct adopt_task_node *task)
 {
+// start 
+// 不用处理
+// end
 	return 0;
 }
 
 int alive_vms_offline(void *user, union virtual_system **VMS, struct adopt_task_node *task)
 {
+       //start
+	   struct residue_package package; // 用于提取来自io thread 中的buffer.
+	   pop_residue_pakage(&package);  // 从io thread 提取.
+	   char *key = hkey_find_key(task->cid.fd); // 从cid 中找对应的key.
+	   hkey_insert_value(key, &package);  // 把key 对应的package 存储起来.
+	   //end
 	return 0;
 }
 
---
+++ programs/mfptpSvp/src/hkey.c
@@ -0,0 +1,211 @@
+#include "hkey.h"
+#include "loger.h"
+#include "string.h"
+
+#define HKEY_SIZE 100
+
+static kv_handler_t *g_hkey = NULL;
+void hkey_init()
+{
+	g_hkey = kv_create(NULL);
+}
+
+static void _int2string(int value, char *buf)
+{
+	snprintf(buf, 10, "%d", value);
+}
+
+void hkey_insert_fd(char *key, int fd)
+{
+	char buf[10] = {};
+	_int2string(fd, buf);
+	char cmd[HKEY_SIZE] = "hset ";
+	strcat(cmd, key);
+	strcat(cmd, " fd ");
+	strcat(cmd, buf);
+	kv_answer_t *ans = kv_ask(g_hkey, cmd, strlen(cmd));
+	if (ans->errnum != ERR_NONE) {
+		error("insert fd error");
+	}
+	kv_answer_release(ans);
+
+	memset(cmd, 0, HKEY_SIZE);
+	memcpy(cmd, "hset fd ", 8);	
+	strcat(cmd, buf);
+	strcat(cmd, " ");
+	strcat(cmd, key);
+	ans = kv_ask(g_hkey, cmd, strlen(cmd));
+	if (ans->errnum != ERR_NONE) {
+		error("insert fd--key error");
+	}
+	kv_answer_release(ans);
+}
+
+int hkey_get_fd(char *key)
+{
+	char cmd[HKEY_SIZE] = "hget ";
+	strcat(cmd, key);
+	strcat(cmd, " fd");
+	kv_answer_t *ans = kv_ask(g_hkey, cmd, strlen(cmd));
+	if (ans->errnum != ERR_NONE) {
+		warn("get key:%s error.", key);
+		kv_answer_release(ans);
+		return -1;
+	}
+	kv_answer_value_t *value = kv_answer_first_value(ans);
+	int fd = atoi((char *)value->ptr);
+	kv_answer_release(ans);
+	return fd;
+}
+
+void hkey_del_fd(char *key)
+{
+	char cmd[HKEY_SIZE] = "hdel ";
+	strcat(cmd, key);
+	strcat(cmd, " fd");
+	kv_answer_t *ans = kv_ask(g_hkey, cmd, strlen(cmd));
+
+	if (ans->errnum != ERR_NONE) {
+		error("delete fd failed");
+	}
+	kv_answer_release(ans);
+}
+
+void hkey_insert_value(char *key, char *value)
+{
+	if (value == NULL) {
+		return;
+	}
+	char cmd[HKEY_SIZE] = "hset ";
+	strcat(cmd, key);
+	strcat(cmd, " value ");
+	strcat(cmd,	value);
+	kv_answer_t *ans = kv_ask(g_hkey, cmd, strlen(cmd));
+	if (ans->errnum != ERR_NONE) {
+		error("insert value error");
+	}
+	kv_answer_release(ans);
+}
+
+char *hkey_get_value(char *key)
+{
+	char cmd[HKEY_SIZE] = "hget ";
+	strcat(cmd, key);
+	strcat(cmd, " value");
+	kv_answer_t *ans = kv_ask(g_hkey, cmd, strlen(cmd));
+	if (ans->errnum != ERR_NONE) {
+		error("get key:%s error.", key);
+		kv_answer_release(ans);
+		return -1;
+	}
+	kv_answer_value_t *value = kv_answer_first_value(ans);
+	char *_value = (char *)malloc(value->ptrlen + 1);
+	int i = 0;
+	for (; i < value->ptrlen; i++) {
+		_value[i] = ((char *) value->ptr)[i];
+	}
+	_value[i] = '\0';
+	kv_answer_release(ans);
+	return _value;
+}
+
+void hkey_del_value(char *key)
+{
+	char cmd[HKEY_SIZE] = "hdel ";
+	strcat(cmd, key);
+	strcat(cmd, " value");
+	kv_answer_t *ans = kv_ask(g_hkey, cmd, strlen(cmd));
+	if (ans->errnum != ERR_NONE) {
+		error("delete value failed");
+	}
+	kv_answer_release(ans);
+}
+
+void hkey_insert_offset(char *key, int offset)
+{
+	char buf[10] = {};
+	_int2string(offset, buf);
+	char cmd[HKEY_SIZE] = "hset ";
+	strcat(cmd, key);
+	strcat(cmd, " offset ");
+	strcat(cmd, buf);
+	kv_answer_t *ans = kv_ask(g_hkey, cmd, strlen(cmd));
+	if (ans->errnum != ERR_NONE) {
+		error("insert offset error");
+	}
+	kv_answer_release(ans);
+}
+
+int hkey_get_offset(char *key)
+{
+	char cmd[HKEY_SIZE] = "hget ";
+	strcat(cmd, key);
+	strcat(cmd, " offset");
+	kv_answer_t *ans = kv_ask(g_hkey, cmd, strlen(cmd));
+	if (ans->errnum != ERR_NONE) {
+		error("get key:%s error.", key);
+		kv_answer_release(ans);
+		return 0;
+	}
+	kv_answer_value_t *value = kv_answer_first_value(ans);
+	int offset = atoi((char *)value->ptr);
+	kv_answer_release(ans);
+	return offset;
+}
+
+void hkey_del_offset(char *key)
+{
+	char cmd[HKEY_SIZE] = "hdel ";
+	strcat(cmd, key);
+	strcat(cmd, " offset");
+	kv_answer_t *ans = kv_ask(g_hkey, cmd, strlen(cmd));
+
+	if (ans->errnum != ERR_NONE) {
+		error("delete offset failed");
+	}
+	kv_answer_release(ans);
+}
+
+void hkey_destroy()
+{
+	kv_destroy(g_hkey);
+}
+
+char *hkey_find_key(int fd)
+{
+	char cmd[HKEY_SIZE] = "hget fd ";
+	char buf[10] = {};
+	_int2string(fd, buf);
+	strcat(cmd, buf);
+	kv_answer_t *ans = kv_ask(g_hkey, cmd, strlen(cmd));
+	if (ans->errnum != ERR_NONE) {
+		error("find key error.");
+		kv_answer_release(ans);
+		return NULL;
+	}
+	kv_answer_value_t *value = kv_answer_first_value(ans);
+	log("value->ptrlen:%d", value->ptrlen);
+	char *_value = (char *)malloc(value->ptrlen + 1);
+	int i = 0;
+	for (; i < value->ptrlen; i++) {
+		_value[i] = ((char *) value->ptr)[i];
+	}
+	_value[i] = '\0';
+	kv_answer_release(ans);
+	return _value;
+}
+
+void hkey_del_fd_key(int fd)
+{
+	char cmd[HKEY_SIZE] = "hdel fd ";
+	char buf[10] = {};
+	_int2string(fd, buf);
+	strcat(cmd, buf);
+	kv_answer_t *ans = kv_ask(g_hkey, cmd, strlen(cmd));
+	if (ans->errnum != ERR_NONE) {
+		error("delete fd key failed");
+	}
+	kv_answer_release(ans);
+
+}
+
