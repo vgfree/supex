9923869a912bc429fdc37b39cc2185c62137294b
baoxue <459032139@qq.com>
2016-07-15 17:53:47 +0800

    Revert "续传为代码"
    This reverts commit 9766c446854187423136145d229326fabd364828
    
    Change-Id: I66dbff60f07b31d68653c313a8b04748134cdda4

--- programs/mttpSvp/src/alive_cpp_api.c
+++ programs/mttpSvp/src/alive_cpp_api.c
@@ -330,27 +330,6 @@ int alive_vms_call(void *user, union virtual_system **VMS, struct adopt_task_nod
 	g_sniff_cfg_list.task_report(p_alive_worker->mount, &sniff_task);
 	return 0;
 
-// start
-    key = // 从客户端接收的数据提取key.
-	fd = hkey_get_fd(key); // 通过接收到的key 寻找cid.
-	if (fd != -1) // 如该cid 存在， 那么可能之前有数据存储。
-	{
-		struct residue_package package;
-		char *value = hkey_get_value(key); // 从对应的libkv 中取得之前存的package.
-		int offset = hkey_get_offset(key); //数据的偏移.
-		init_residue_package(&package, task->cid.fd, offset, value, strlen(value)); // 不支持二进制。
-		push_residue_package(&package);// 把package 重新存到io thread 的buffer.
-		hkey_del_fd(key); // 把之前存储的所有对应内容清空。
-		hkey_del_value(key);
-		hkey_del_fd_key(fd);
-		destroy_residue_package(&package);
-	}
-	else {
-		// 如果不存在， 那么是第一次握手，不用处理。
-	}
-	hkey_insert_fd(key, cid.fd); // 存key, cid. 更新对应的key 与cid 的bind.
-// end
-
 ERROR:
 	mttpsvp_libkv_del(task->cid, task->sfd);
 	alive_close_conn(1, task->cid, task->sfd);
@@ -359,9 +338,6 @@ ERROR:
 
 int alive_vms_online(void *user, union virtual_system **VMS, struct adopt_task_node *task)
 {
-	// start
-	// 不用处理。
-	// end
 	mttpsvp_libkv_set(task->cid, task->sfd, "0");
 	return 0;
 }
@@ -369,12 +345,6 @@ int alive_vms_online(void *user, union virtual_system **VMS, struct adopt_task_n
 int alive_vms_offline(void *user, union virtual_system **VMS, struct adopt_task_node *task)
 {
 	mttpsvp_libkv_del(task->cid, task->sfd);
-	//start
-	struct residue_package package; // 用于提取来自io thread 中的buffer.
-	pop_residue_pakage(&package);  // 从io thread 提取。
-	char *key = hkey_find_key(task->cid.fd); // 从cid 中找对应的key.
-	hkey_insert_value(key, &package);  // 把key 对应的package 存储起来.
-	//end
 	return 0;
 }
 
